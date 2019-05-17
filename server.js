c2d018d4e628de790cbc9ebb21dc99ae27edc06c
Max Stern <stern.maxwell@gmail.com>
2019-05-17 13:11:21 -0700

    server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`started on port ${port}`);
+})
