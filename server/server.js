6a54ff2db365ff81ec6f7f62d18995ab77f309c8
Max Stern <stern.maxwell@gmail.com>
2019-05-17 14:11:00 -0700

    bcrypt hash test

--- server/server.js
+++ server/server.js
@@ -10,6 +10,20 @@ const { User } = require('./models/user');
 
 app.use(bodyParser.json());
 
+app.post('/api/user', (req,res) => {
+    
+    const user = new User({
+        email: req.body.email,
+        password: req.body.password
+    })
+
+    user.save((err,doc) => {
+        if(err) res.status(400).send(err)
+        res.status(200).send(doc)
+    })
+})
+
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
