cd999fc80336e4862e330627a94081a98e086bab
Max Stern <stern.maxwell@gmail.com>
2019-05-17 13:13:25 -0700

    mongoose.connect()

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://locahost:27017/auth');
+
+
 
 const port = process.env.PORT || 3000;
 
