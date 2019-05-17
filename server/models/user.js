0d755761e00e886e5bf0843de3a3731acc4c5627
Max Stern <stern.maxwell@gmail.com>
2019-05-17 14:37:44 -0700

    hashing passwords

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const SALT_I = 10; // salt iteration
 
 const userSchema = mongoose.Schema({
     email: {
@@ -11,6 +13,25 @@ const userSchema = mongoose.Schema({
         type: String,
         required: true,
         minlength: 6      
+    }
+})
+
+userSchema.pre('save', (next) => {
+    var user = this;
+    
+    // check if password was modified
+    if(user.isModified('password')){
+        bcrypt.genSalt(SALT_I, (err,salt) => {
+            if(err) return next(err);
+    
+            bcrypt.hash(user.password, salt, (err,hash) => {
+                if(err) return next(err);
+                user.password = hash;
+                next();
+            })
+        })
+    } else {
+        next();
     }
 })
 
