fe7bd9ea9b17c11ec49de00193a0f2dce037a349
Max Stern <stern.maxwell@gmail.com>
2019-05-17 13:29:05 -0700

    user model

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
 const app = express();
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://locahost:27017/auth');
 
+const { User } = require('./models/user');
 
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 3000;
 
