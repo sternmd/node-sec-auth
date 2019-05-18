6b9d9f5ce399d90978cc2f4bec4258f61d51677e
Max Stern <stern.maxwell@gmail.com>
2019-05-18 10:53:26 -0700

    crypto-js testing

--- hash.js
+++ hash.js
@@ -1,10 +1,27 @@
 const bcrypt = require('bcrypt');
+const {MD5} = require('crypto-js');
 
-bcrypt.genSalt(10, (err,salt)=> {
-    if(err) return next(err);
+// bcrypt.genSalt(10, (err,salt)=> {
+//     if(err) return next(err);
 
-    bcrypt.hash('password123', salt, (err,hash) => {
-        if(err) return next(err);
-        console.log(hash)
-    })
-})
+//     bcrypt.hash('password123', salt, (err,hash) => {
+//         if(err) return next(err);
+//         console.log(hash)
+//     })
+// })
+
+const secret = 'mysecretpassword';
+const secretSalt = 'dfihuvdS8324husdk'
+
+const user = {
+    id: 1,
+    token: MD5('password').toString() + secretSalt
+}
+
+const receivedToken = '5f4dcc3b5aa765d61d8327deb882cf99dfihuvdS8324husdk';
+
+if (receivedToken === user.token) {
+    console.log('move forward')
+}
+
+console.log(user);
