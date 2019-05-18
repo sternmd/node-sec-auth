adef360c160dd8ee062b75338d6fd44af1bd62ee
Max Stern <stern.maxwell@gmail.com>
2019-05-18 11:01:20 -0700

    jsonwebtoken test

--- hash.js
+++ hash.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
 const {MD5} = require('crypto-js');
+const jwt = require('jsonwebtoken');
 
 // bcrypt.genSalt(10, (err,salt)=> {
 //     if(err) return next(err);
@@ -10,18 +11,30 @@ const {MD5} = require('crypto-js');
 //     })
 // })
 
-const secret = 'mysecretpassword';
-const secretSalt = 'dfihuvdS8324husdk'
+// const secret = 'mysecretpassword';
+// const secretSalt = 'dfihuvdS8324husdk'
 
-const user = {
-    id: 1,
-    token: MD5('password').toString() + secretSalt
-}
+// const user = {
+//     id: 1,
+//     token: MD5('password').toString() + secretSalt
+// }
 
-const receivedToken = '5f4dcc3b5aa765d61d8327deb882cf99dfihuvdS8324husdk';
+// const receivedToken = '5f4dcc3b5aa765d61d8327deb882cf99dfihuvdS8324husdk';
 
-if (receivedToken === user.token) {
-    console.log('move forward')
-}
+// if (receivedToken === user.token) {
+//     console.log('move forward')
+// }
 
-console.log(user);
+// console.log(user);
+
+const id = '1000';
+const secret = 'supersecret';
+
+const token = jwt.sign(id, secret);
+
+const receivedToken = 'eyJhbGciOiJIUzI1NiJ9.MTAwMA.L9PmEqLlZjettygguzj25agunJu6NkvVtG9RFRBnK2Y';
+
+const decodeToken = jwt.verify(receivedToken, secret);
+
+console.log(decodeToken);
+
